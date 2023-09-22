1894b5272acaad0c2b1a92bf97efa3217e49c426
jaschamerle <jascha.merle@gmail.com>
2023-09-22 09:30:22 +0200

    feat: missing inclusion. ThicknessError.swift containing exceptions pertaining to Thickness.swift

---
+++ Sources/Rapids/base/errors/ThicknessError.swift
@@ -0,0 +1,18 @@
+//
+// Created by Jascha MERLE on 07/08/2023.
+//
+
+import Foundation
+
+/// Represents errors related to thickness validation.
+///
+/// The `ThicknessError` enum is used to indicate errors that can occur during the validation
+/// of thickness values in your application.
+enum ThicknessError: Error {
+
+    /// Indicates that the provided thickness is invalid.
+    ///
+    /// This thickness is invalid because it could not be parsed from the provided string.
+    case invalidThickness
+}
+
